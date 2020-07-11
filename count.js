2c25067d17f8985fc34ad590849f0bc7a12a5256
Sakshi Jangra <sakshijangra111000@gmail.com>
2020-07-11 22:27:32 +0530

    change

--- count.js
+++ count.js
@@ -19,9 +19,13 @@ let countDown = new Date('July 12, 2020 00:00:00').getTime(),
         (distance % minute) / second
       ));
 
-    //do something later when date is reached
-    //if (distance < 0) {
-    //  clearInterval(x);
-    //  'IT'S MY BIRTHDAY!;
-    //}
+    if (distance < 0) {
+      clearInterval(x);
+      ("IT'S YOUR BIRTHDAY!");
+      var img = document.createElement('img');
+      img.src = 'party.gif';
+      var src = document.getElementById('x');
+      src.appendChild(img);
+      distance = 0;
+    }
   }, second);
